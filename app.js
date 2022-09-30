cead22f9597f4bc79d71d4181b1bdf530e4ff4d7
Andree Rojas <andre_r_g@hotmail.com>
2022-09-30 19:59:42 +0200

    created first layout for the inputs

---
+++ app.js
@@ -0,0 +1,16 @@
+// const customRadio = document.querySelector('#tip-custom');
+
+// customRadio.addEventListener('input',function(){
+//     const customContainer = customRadio.parentElement;
+    
+//     if(customRadio.checked){
+//         console.log("im checked");
+//         customRadio.nextElementSibling.classList.add('hidden');
+//         customContainer.lastElementChild.classList.remove('hidden');
+//     }
+//     else{
+//         console.log("im unchecked");
+//         customRadio.nextElementSibling.classList.remove('hidden');
+//         customContainer.lastElementChild.classList.add('hidden');
+//     }
+// })
