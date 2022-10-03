4417bf8e550b47d2c940e47d623e356f2d696c3d
Andree Rojas <andre_r_g@hotmail.com>
2022-10-03 12:28:55 +0200

    finished layout and number of people validation

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const customTipInput = document.querySelector('#tip-custom-text-input');
+const numPeople = document.querySelector('#people');
+const resetBtn = document.querySelector('#btn-reset');
 
-customTipInput.addEventListener('focus',function(){
+customTipInput.addEventListener('focus', function () {
     let radioChecked = document.querySelector('input[type="radio"]:checked');
-    if(radioChecked){
+    if (radioChecked) {
         radioChecked.checked = false;
     }
+})
+
+numPeople.addEventListener('focusout', function () {
+    let container = numPeople.parentElement; 
+    let errorMsg = container.previousElementSibling.lastElementChild;
+    if (numPeople.value !== "" && parseFloat(numPeople.value) !== 0) {
+        resetBtn.disabled = false;
+        errorMsg.classList.add('hidden');
+        container.classList.remove('wrong-input');
+    }
+    else {
+        errorMsg.classList.remove('hidden');
+        container.classList.add('wrong-input');
+    }
+})
+
+numPeople.addEventListener('focusin',function(){
+    let errorMsg = numPeople.parentElement.previousElementSibling.lastElementChild;
+    errorMsg.classList.add('hidden');
 })
